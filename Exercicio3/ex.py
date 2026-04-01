61c67071367269aea20ee93e09e027ab7a5cb679
nicmacedo1 <nicolas.fnm899@gmail.com>
2026-03-31 21:55:43 -0300

    Exercicio3

---
+++ Exercicio3/ex.py
@@ -0,0 +1,13 @@
+primeiro_numero = int(input("Digite o primeiro numero inteiro: "))
+segundo_numero = int(input("Digite o segundo numero inteiro: "))
+
+soma = primeiro_numero + segundo_numero
+print(soma)
+subtracao = primeiro_numero - segundo_numero
+print(subtracao)
+multiplicacao = primeiro_numero * segundo_numero
+print(multiplicacao)
+divisao = primeiro_numero // segundo_numero
+print(divisao)
+resto_divisao = primeiro_numero % segundo_numero
+print(resto_divisao)
