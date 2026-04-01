a47327d64458154a44babeb10a0c3ca3e5a45e09
nicmacedo1 <nicolas.fnm899@gmail.com>
2026-03-31 21:59:39 -0300

    Exercicio4

---
+++ Exercicio4/ex.py
@@ -0,0 +1,6 @@
+primeira_nota = float(input("Digite a primeira nota: "))
+segunda_nota = float(input("Digite a segunda nota: "))
+terceira_nota = float(input("Digite a terceira nota: "))
+
+media = primeira_nota + segunda_nota + terceira_nota / 3
+print(media)
